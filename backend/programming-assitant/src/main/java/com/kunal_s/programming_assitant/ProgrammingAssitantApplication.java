a136c317b5a0c90320e9d56a87a602e0ac3d7167
Kunals990 <kunalsable9990@gmail.com>
2025-03-15 11:09:27 +0530

    Base project is setup,basic frontend and backend is created

---
+++ backend/programming-assitant/src/main/java/com/kunal_s/programming_assitant/ProgrammingAssitantApplication.java
@@ -0,0 +1,13 @@
+package com.kunal_s.programming_assitant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingAssitantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingAssitantApplication.class, args);
+	}
+
+}
---
+++ backend/programming-assitant/src/test/java/com/kunal_s/programming_assitant/ProgrammingAssitantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal_s.programming_assitant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingAssitantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
